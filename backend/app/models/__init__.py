7ef4c29fa5d706819c34fe0c637b88702ce73dec
ISHANT57 <ishantbhoyar59@gmail.com>
2026-07-21 13:12:12 +0530

    M10: register Document model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@
 from app.models.organization import Organization  # noqa: F401
 from app.models.user import User  # noqa: F401
 from app.models.collection import Collection  # noqa: F401
+from app.models.document import Document  # noqa: F401
